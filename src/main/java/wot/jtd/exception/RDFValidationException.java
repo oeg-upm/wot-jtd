174a57ba90e4919eb0d8aab446dd37f22cbd3fbf
cimmino <cimmino@fi.upm.es>
2021-02-19 11:35:49 +0100

    fixed pom

---
+++ src/main/java/wot/jtd/exception/RDFValidationException.java
@@ -0,0 +1,11 @@
+package wot.jtd.exception;
+
+public class RDFValidationException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public RDFValidationException(String message) {
+		super(message);
+	}
+
+}
